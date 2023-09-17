d647ad662c937ba78117d4fe856d7e2347c56512
Ximo <joaquin.rovira97@gmail.com>
2023-09-17 15:54:20 +0200

    Add TZ (TimeZone) configuration value

--- Configuration/GeneralConfig.cs
+++ Configuration/GeneralConfig.cs
@@ -10,6 +10,22 @@ public class GeneralConfig
     [Required]
     public required string Password { get; set; }
 
+
+    private string _TZ = TimeZoneInfo.Utc.Id;
+    public string TZ
+    {
+        get => _TZ;
+        init
+        {
+            // NOTE: throws for invalid/unkown timezone strings
+            var info = Functional.Catch(() => TimeZoneInfo.FindSystemTimeZoneById(value)).GetOrThrow();
+            _TZ = info.Id;
+        }
+    }
+
+
+
+
     public HostEnvironment Environment { get; set; } = HostEnvironment.Development;
 }
 
--- Util/CSharpFunctionalExtensions.cs
+++ Util/CSharpFunctionalExtensions.cs
@@ -2,26 +2,18 @@ namespace CSharpFunctionalExtensions;
 
 public static class MoreCSharpFunctionalExtensions
 {
-    public static UnitResult<E> ToUnitResult<T, E>(this Result<T, E> r)
-    {
-        if (r.IsFailure) return UnitResult.Failure(r.Error);
-        return UnitResult.Success<E>();
-    }
-    public static async Task<UnitResult<E>> ToUnitResult<T, E>(this Task<Result<T, E>> r) => ToUnitResult(await r);
-
-    public static UnitResult<string> ToUnitResult<T>(this Result<T> r)
-    {
-        if (r.IsFailure) return UnitResult.Failure(r.Error);
-        return UnitResult.Success<string>();
-    }
-    public static async Task<UnitResult<string>> ToUnitResult<T>(this Task<Result<T>> r) => ToUnitResult(await r);
-
     public static void SuccessOrThrow<T>(this UnitResult<T> r) where T : Error
     {
         if (r.IsFailure) throw r.Error;
         return;
     }
     public static async Task SuccessOrThrow<T>(this Task<UnitResult<T>> r) where T : Error => SuccessOrThrow(await r);
+    public static T GetOrThrow<T,E>(this Result<T,E> r) where E : Error
+    {
+        if (r.IsFailure) throw r.Error;
+        return r.Value;
+    }
+    public static async Task GetOrThrow<T,E>(this Task<Result<T,E>> r) where E : Error => GetOrThrow(await r);
 }
 
 public static class Functional

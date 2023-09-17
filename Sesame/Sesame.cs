ab71c6102b8213001b29de1c8f6ce7443f4cc681
Ximo <joaquin.rovira97@gmail.com>
2023-09-17 12:25:21 +0200

    Randomize headers

--- Sesame/Sesame.cs
+++ Sesame/Sesame.cs
@@ -24,13 +24,23 @@ public class Sesame : IDisposable {
     public Task<UnitResult<Error>> CheckOut(UserInfo u, Maybe<SiteInfo> s) => CheckOut(u.id, s.Map(s=> s.id));
     public Task<UnitResult<Error>> CheckOut(string userId, Maybe<string> siteId) => new CheckOut(HttpClient(), Context).Do(userId, siteId);
     public void Dispose() { HttpClientHandler.Dispose(); }
+    
+    public static readonly string[] AndroidVersions = { "10", "11", "12", "13" };
+    public static readonly string[] FirefoxVersions = { "117.0.1", "116.0.3", "115.2.1", "114.0.2" };
+    private string UserAgent() {
+        var AndroidVersion = AndroidVersions[Random.Shared.Next(AndroidVersions.Length)];
+        var FirefoxVersion = FirefoxVersions[Random.Shared.Next(FirefoxVersions.Length)];
+        return $"Mozilla/5.0 (Android {AndroidVersion}; Mobile; rv:{FirefoxVersion}) Gecko/{FirefoxVersion} Firefox/{FirefoxVersion}";
+    }
+    private string RSRC() => (Random.Shared.Next(10) + 20).ToString();
+
     HttpClient HttpClient() {
         var client = new HttpClient(HttpClientHandler) {};
-        client.DefaultRequestHeaders.Add("User-Agent","Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0");
+        client.DefaultRequestHeaders.Add("User-Agent", UserAgent());
         client.DefaultRequestHeaders.Add("Accept","application/json");
         client.DefaultRequestHeaders.Add("Accept-Language","en-GB,en;q=0.5");
         client.DefaultRequestHeaders.Add("Accept-Encoding","gzip, deflate, br");
-        client.DefaultRequestHeaders.Add("rsrc","31");
+        client.DefaultRequestHeaders.Add("rsrc",RSRC());
         client.DefaultRequestHeaders.Add("traceparent", Guid.NewGuid().ToString());
         client.DefaultRequestHeaders.Add("Origin","https://app.sesametime.com");
         client.DefaultRequestHeaders.Add("Referer","https://app.sesametime.com/");

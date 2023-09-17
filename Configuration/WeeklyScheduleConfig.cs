574bd4277eb931a6ac670d92d54a548c57ebdbaa
Ximo <joaquin.rovira97@gmail.com>
2023-09-17 11:41:53 +0200

    Fix WeeklySchedultConfig.ToDict()

--- Configuration/WeeklyScheduleConfig.cs
+++ Configuration/WeeklyScheduleConfig.cs
@@ -14,13 +14,13 @@ public class WeeklyScheduleConfig
     public IDictionary<DayOfWeek, (TimeOfDay, TimeOfDay)> ToDict() 
     {
         var dict = new Dictionary<DayOfWeek, (TimeOfDay, TimeOfDay)>();
-        if (Monday is not null) dict.Add(DayOfWeek.Monday, new(new(Monday.Start!.Hour, Monday.Start.Hour), new(Monday.End!.Hour, Monday.End.Hour)));
-        if (Tuesday is not null) dict.Add(DayOfWeek.Tuesday, new(new(Tuesday.Start!.Hour, Tuesday.Start.Hour), new(Tuesday.End!.Hour, Tuesday.End.Hour)));
-        if (Wednesday is not null) dict.Add(DayOfWeek.Wednesday, new(new(Wednesday.Start!.Hour, Wednesday.Start.Hour), new(Wednesday.End!.Hour, Wednesday.End.Hour)));
-        if (Thursday is not null) dict.Add(DayOfWeek.Thursday, new(new(Thursday.Start!.Hour, Thursday.Start.Hour), new(Thursday.End!.Hour, Thursday.End.Hour)));
-        if (Friday is not null) dict.Add(DayOfWeek.Friday, new(new(Friday.Start!.Hour, Friday.Start.Hour), new(Friday.End!.Hour, Friday.End.Hour)));
-        if (Saturday is not null) dict.Add(DayOfWeek.Saturday, new(new(Saturday.Start!.Hour, Saturday.Start.Hour), new(Saturday.End!.Hour, Saturday.End.Hour)));
-        if (Sunday is not null) dict.Add(DayOfWeek.Sunday, new(new(Sunday.Start!.Hour, Sunday.Start.Hour), new(Sunday.End!.Hour, Sunday.End.Hour)));
+        if (Monday is not null) dict.Add(DayOfWeek.Monday, new(new(Monday.Start!.Hour, Monday.Start.Minute), new(Monday.End!.Hour, Monday.End.Minute)));
+        if (Tuesday is not null) dict.Add(DayOfWeek.Tuesday, new(new(Tuesday.Start!.Hour, Tuesday.Start.Minute), new(Tuesday.End!.Hour, Tuesday.End.Minute)));
+        if (Wednesday is not null) dict.Add(DayOfWeek.Wednesday, new(new(Wednesday.Start!.Hour, Wednesday.Start.Minute), new(Wednesday.End!.Hour, Wednesday.End.Minute)));
+        if (Thursday is not null) dict.Add(DayOfWeek.Thursday, new(new(Thursday.Start!.Hour, Thursday.Start.Minute), new(Thursday.End!.Hour, Thursday.End.Minute)));
+        if (Friday is not null) dict.Add(DayOfWeek.Friday, new(new(Friday.Start!.Hour, Friday.Start.Minute), new(Friday.End!.Hour, Friday.End.Minute)));
+        if (Saturday is not null) dict.Add(DayOfWeek.Saturday, new(new(Saturday.Start!.Hour, Saturday.Start.Minute), new(Saturday.End!.Hour, Saturday.End.Minute)));
+        if (Sunday is not null) dict.Add(DayOfWeek.Sunday, new(new(Sunday.Start!.Hour, Sunday.Start.Minute), new(Sunday.End!.Hour, Sunday.End.Minute)));
         return dict;
     }
 

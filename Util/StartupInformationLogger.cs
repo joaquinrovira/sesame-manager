7c2e7fb262800ca74a0825c069e3fecc98b820ab
Ximo <joaquin.rovira97@gmail.com>
2023-09-20 19:40:43 +0200

    Update config logging

--- Util/StartupInformationLogger.cs
+++ Util/StartupInformationLogger.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Hosting;
 
@@ -8,40 +9,39 @@ public record class StartupInformationLogger(
     IOptions<GeneralConfig> GeneralConfig,
     IOptions<WeeklyScheduleConfig> WeeklyScheduleConfig,
     IOptions<AdditionalHolidaysConfig> AdditionalHolidaysConfig,
+    IOptions<IdealHolidayProviderConfiguration> IdealHolidayProviderConfiguration,
+    IOptions<NagerHolidayProviderConfiguration> NagerHolidayProviderConfiguration,
     DateTimeService DateTimeService
 ) : IHostedService
 {
+    List<ConfigMessageItem> Sections = new List<ConfigMessageItem>{
+        new("TimeZone", DateTimeService.TimeZone, true),
+        new("Weekly schedule", WeeklyScheduleConfig.Value),
+        new("Holiday Provider - nager.com", NagerHolidayProviderConfiguration.Value),
+        new("Holiday Provider - ideal.es", IdealHolidayProviderConfiguration.Value),
+        new("Additional holidays", AdditionalHolidaysConfig.Value),
+    };
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        var (message, parameters) = ConfigurationMessage();
-        Logger.LogInformation(message, parameters);
+        Logger.LogInformation(ConfigurationMessage());
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
-    private (string, object?[]) ConfigurationMessage()
+    private string ConfigurationMessage()
     {
-        var parameters = new List<object?>
-        {
-            JsonSerializer.Serialize(DateTimeService.TimeZone, new JsonSerializerOptions() { WriteIndented = true }),
-            JsonSerializer.Serialize(WeeklyScheduleConfig.Value, new JsonSerializerOptions() { }),
-            JsonSerializer.Serialize(AdditionalHolidaysConfig.Value, new JsonSerializerOptions() { })
-        };
-        var message = @"============= CONFIGURATION =============
-
-TimeZone:
-{timezone}
-
-Weekly schedule:
-{schedule}
-
-Additional holidays:
-{holidays}
-
-=========================================
-";
-        return (message, parameters.ToArray());
+        var builder = new StringBuilder();
+        builder.AppendLine("============= CONFIGURATION =============");
+        Sections.ForEach(e => builder.AppendLine(e + "\n"));
+        builder.AppendLine("=========================================");
+        return builder.ToString();
     }
+
+    private record ConfigMessageItem(string name, object value, bool pretty = false) {
+        public override string ToString() => $"{name}:\n{JSON(value, pretty)}";
+        public static implicit operator string(ConfigMessageItem i) => i.ToString();
+    }
+    private static string JSON<T>(T value, bool WriteIndented = false) => JsonSerializer.Serialize(value, new JsonSerializerOptions() { WriteIndented = WriteIndented });
 }
